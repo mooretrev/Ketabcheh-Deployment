28b61c501a45b01a18578a4a87ff11f953c6f83b
Trevor Moore <trevor.moore1999@gmail.com>
2019-11-07 17:21:11 -0600

    added olins file

---
+++ keta/ProcessRequest.py
@@ -0,0 +1,10 @@
+
+'''
+    Given an isbn number, return an array of the related info about the book
+'''
+def isbn_search(isbn):
+    book_info = []
+
+    # here you make your api call and put into an array using python or javascript
+
+    return book_info
