7dbd39eb8a5391a5fa6c6c18c6f5ed15b94d17a9
Trevor Moore <trevor.moore1999@gmail.com>
2019-11-07 11:38:53 -0600

    Forms added to index html, however formmated weird

--- keta/forms.py
+++ keta/forms.py
@@ -1,8 +1,22 @@
 from django import forms
 
+
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
 
 class SearchForm(forms.Form):
+    ADVANCE_SEARCH_CHOICES = [
+        ('isbn', 'ISBN(10 or 13)'),
+        ('title', 'Title'),
+        ('author', 'Author'),
+        ('keywords', 'Keywords'),
+    ]
+    # ADVANCE_SEARCH_CHOICES = [
+    #     ('blue', 'Blue'),
+    #     ('green', 'Green'),
+    #     ('black', 'Black'),
+    # ]
+
     search_text = forms.CharField(label="Search", max_length=100)
-    advance_serach = forms.MultipleChoiceField(label="Advance Search", )
+    advance_search = forms.MultipleChoiceField(label="Advance Search", initial=['ISBN'], widget=forms.RadioSelect,  choices=ADVANCE_SEARCH_CHOICES)
+
