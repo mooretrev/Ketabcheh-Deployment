84d38e1b12bfbcb3736d82f5e46d9eed2791488b
Trevor Moore <trevor.moore1999@gmail.com>
2019-11-07 11:16:58 -0600

    Working on Adding forms to the HTML Search

--- keta/forms.py
+++ keta/forms.py
@@ -2,3 +2,7 @@ from django import forms
 
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
+
+class SearchForm(forms.Form):
+    search_text = forms.CharField(label="Search", max_length=100)
+    advance_serach = forms.MultipleChoiceField(label="Advance Search", )
