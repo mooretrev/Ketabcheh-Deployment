9d3d0bd82a1057dd91af2d00fa33236fa220a2b9
Olin Zhou <olin.zhou.0@gmail.com>
2019-11-11 19:21:44 -0600

    api call

---
+++ api/call.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+
+key = "AIzaSyDgszPoXualhFKbDKCNxXXn3cdOhLKgi6A";
+isbn = "0470430907";
+
+axios
+  .get(
+    `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=AIzaSyDgszPoXualhFKbDKCNxXXn3cdOhLKgi6A`
+  )
+
+  .then(response => {
+    console.log(response.data.items[0]);
+    console.log("\nTitle:\n");
+    console.log(response.data.items[0].volumeInfo.title);
+    // console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
