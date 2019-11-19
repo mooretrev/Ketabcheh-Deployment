1af72731abac7be7d75c315b3c38409fc41e3121
Trevor Moore <trevor.moore1999@gmail.com>
2019-11-19 08:47:17 -0600

    working Book Run API call

--- keta/ProcessRequest.py
+++ keta/ProcessRequest.py
@@ -6,28 +6,37 @@ import json
     
     Use json loads
 '''
-def books_search_by_title(title):
-    key = "AIzaSyDgszPoXualhFKbDKCNxXXn3cdOhLKgi6A"
-
-    url = r'https://www.googleapis.com/books/v1/volumes?q=title:{}&key={}'.format(title, key)
+def process_api(url):
     response = requests.get(url)
     book_info = json.loads(response.text)
 
     return book_info
+
+def books_search_by_title(title):
+    key = "AIzaSyDgszPoXualhFKbDKCNxXXn3cdOhLKgi6A"
+    url = r'https://www.googleapis.com/books/v1/volumes?q=title:{}&key={}'.format(title, key)
+    return process_api(url)
 
 def books_search_by_isbn(isbn):
     key = "AIzaSyDgszPoXualhFKbDKCNxXXn3cdOhLKgi6A"
 
     url = r'https://www.googleapis.com/books/v1/volumes?q=isbn:{}&key={}'.format(isbn, key)
-    response = requests.get(url)
-    book_info = json.loads(response.text)
+    return process_api(url)
 
-    return book_info
+def book_run_api_call(isbn):
+    price_key = 'hemn2x0o9xwbe7bfz2jz'
+    affiliate_key = '5391'
+
+    url = r'http://booksrun.com/api/price/sell/{}?key={}'.format(isbn, price_key)
+    return process_api(url)
 
 
-result = books_search_by_title("hungery games")
-print(result)
+
+# result = books_search_by_title("hungery games")
+# print(result)
 # print(result['items'][0])
+
+print(book_run_api_call('1464108730'))
 
 
 
