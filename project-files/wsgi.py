6d524aafb282880700e885a39e4ca5b63a6e5335
Trevor Moore <moore.trev@tamu.edu>
2019-12-03 22:00:08 -0600

    trying to solve the static file issues

--- project-files/wsgi.py
+++ project-files/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# add your project directory to the sys.path
+project_home = u'/home/ubuntu/Ketabcheh-Deployment'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
+# set environment variable to tell django where your settings.py is
 os.environ['DJANGO_SETTINGS_MODULE'] = 'project-files.settings'
 
+
+# serve django via WSGI
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
